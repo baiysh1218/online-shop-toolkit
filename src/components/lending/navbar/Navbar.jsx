7c69f931670a70d059db78f9fe8fd9cc8d29f256
baiysh1218 <baiyshbeksultanov1@gmail.com>
2023-05-01 11:17:59 +0600

    first commit

---
+++ src/components/lending/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="wrapper__content">
+          <p>Text</p>
+          <p>Text</p>
+          <p>Text</p>
+          <p>Text</p>
+          <p>Text</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
